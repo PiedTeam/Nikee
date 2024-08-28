cea5ebab39a752e2412d0318730bf32661ea55f7
YenMinhHoang <minhhy2897@gmail.com>
2024-08-28 11:35:32 +0700

    Update App component styling

--- nikee-store/src/App.tsx
+++ nikee-store/src/App.tsx
@@ -1,5 +1,7 @@
+import './index.css';
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <h1 className="bg-purple-500 text-5xl">Hello world!</h1>;
 };
 
 export default App;
