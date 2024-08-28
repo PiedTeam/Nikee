a41376f84e2988fc43ee0a8a1b4a6d018d5873de
YenMinhHoang <minhhy2897@gmail.com>
2024-08-28 11:06:11 +0700

    Add Tailwind CSS configuration and styles

--- nikee-store/eslint.config.js
+++ nikee-store/eslint.config.js
@@ -1,22 +1,38 @@
-
-module.exports = {
-  ignores: ['dist'],
-  parser: '@typescript-eslint/parser',
-  extends: [
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-    'eslint-plugin-react-refresh',
-    'eslint-plugin-react-hooks'
-  ],
-  files: ['**/*.{ts,tsx}'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    "indent": ["error", 2],
+import { ESLint } from 'eslint';
+import typescriptEslintParser from '@typescript-eslint/parser';
+import reactPlugin from 'eslint-plugin-react';
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
+import importPlugin from 'eslint-plugin-import';
+import reactRefreshPlugin from 'eslint-plugin-react-refresh';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import prettierPlugin from 'eslint-plugin-prettier';
+export default [
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: typescriptEslintParser,
+    },
+    plugins: {
+      react: reactPlugin,
+      '@typescript-eslint': typescriptEslintPlugin,
+      import: importPlugin,
+      'react-refresh': reactRefreshPlugin,
+      'react-hooks': reactHooksPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      indent: ['error', 2],
+      'react/jsx-indent': ['error', 2],
+      'react/jsx-indent-props': ['error', 2],
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
   },
-};
+];
