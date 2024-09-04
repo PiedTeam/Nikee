12184b89969c49fec5b7024b51a893ae9030ef13
YenMinhHoang <minhhy2897@gmail.com>
2024-09-04 12:44:51 +0700

    Add UI library and dependencies

--- @nikee/nike-bo/src/App.tsx
+++ @nikee/nike-bo/src/App.tsx
@@ -1,3 +1,4 @@
+import { InputPassword } from 'ui-lib';
 import './index.css';
 import { Input } from '@nextui-org/react';
 
@@ -5,7 +6,7 @@ const App = () => {
   return (
     <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
       <Input type="email" label="Email" />
-      <Input type="email" label="Email" placeholder="Enter your email" />
+      <InputPassword/>
     </div>
   );
 };
---
+++ ui-lib/src/index.ts
@@ -0,0 +1,4 @@
+import './index.css';
+
+export { default as InputPassword } from './InputPassword/InputPassword';
+export * from './InputPassword/InputPassword';
